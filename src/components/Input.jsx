6ade4b511754598124f2cb06c656812a5106dd4d
Maria Eduarda <meac21kj@gmail.com>
2025-03-12 18:06:30 -0300

    refact: propriedades do componente para inputs

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,10 +2,7 @@ function Input(props) {
   return (
     <input
       className="border-slate-300 bg-slate-800 text-white font-bold outline-slate-400 px-4 py-2 rounded-md"
-      type={props.type}
-      placeholder={props.placeholder}
-      value={props.value}
-      onChange={props.onChange}
+      {...props}
     ></input>
   );
 }
