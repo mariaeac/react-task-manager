6b1154fdd32c85ff163ee8fcd1ebb97ecd6307eb
Maria Eduarda <meac21kj@gmail.com>
2025-03-12 04:36:47 -0300

    feat: página de detalhes da task com React Router

---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,7 @@
+function TaskDetails() {
+  return (
+    <div className="w-screen h-screen bg-slate-500 rounded-md shdaow-2x1 flex flex-col"></div>
+  );
+}
+
+export default TaskDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter, createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router-dom";
+import TaskDetails from "./components/TaskDetails.jsx";
+
+// const root = document.getElementById("root");
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/task-details",
+    element: <TaskDetails />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}> </RouterProvider>
+  </StrictMode>
+);
