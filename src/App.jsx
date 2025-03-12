2466093c54d315890dee22e2136a69b291496075
Maria Eduarda <meac21kj@gmail.com>
2025-03-12 03:20:54 -0300

    feat: deletar task

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,10 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onDeleteClick(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-900 flex justify-center p-6">
       <div className="w-[500px]">
@@ -38,7 +42,11 @@ function App() {
         </h1>
 
         <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteClick={onDeleteClick}
+        />
       </div>
     </div>
   );
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,4 +1,4 @@
-import { ListCollapse } from "lucide-react";
+import { ListCollapse, Trash2 } from "lucide-react";
 
 function Tasks(taskProps) {
   return (
@@ -8,12 +8,21 @@ function Tasks(taskProps) {
           <li key={task.id} className="flex gap-2">
             <button
               onClick={() => taskProps.onTaskClick(task.id)}
-              className="bg-slate-800 text-white text-left p-2 rounded-md w-full"
+              className={`bg-slate-800 text-white text-left p-2 rounded-md w-full ${
+                task.isCompleted && "line-through"
+              }`}
             >
               {task.title}
             </button>
             <button className="bg-slate-800 text-white p-2  rounded-md">
               <ListCollapse />
+            </button>
+
+            <button
+              onClick={() => taskProps.onDeleteClick(task.id)}
+              className="bg-slate-800 text-white p-2  rounded-md"
+            >
+              <Trash2 />
             </button>
           </li>
         ))}
