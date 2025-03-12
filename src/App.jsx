cf8359f4bc5b27389b49ad11646671c9e5413527
Maria Eduarda <meac21kj@gmail.com>
2025-03-12 02:05:38 -0300

    config: configuração do tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-red-500"> Task Manager </h1>
+      <Tasks />
+      <AddTask />
+    </div>
+  );
+}
+
+export default App;
